4d36c5bbd6a011c78f4065e96ca5f98428a41d78
NekozZa <523H0114@student.tdtu.edu.vn>
2025-03-26 19:20:54 +0700

    complete nutritionist profile

--- index.js
+++ index.js
@@ -6,16 +6,22 @@ const app = express()
 const PORT = 3000
 
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    res.render('index.ejs')
+    res.render('profile.ejs')
+    // res.render('index.ejs')
 })
 
 app.get('/searcher', (req, res) => {
     res.render('./partials/searcher.ejs', {apiKey: process.env.API_KEY})
 })
 
+app.get('/profile', (req, res) => {
+    res.render('profile.ejs')
+})
 
 app.listen(PORT, () => {
     console.log('http://localhost:3000/')
