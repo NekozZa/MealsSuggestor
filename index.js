c5f7f524c84e9ee699c41a3e423faa7e2098ef52
NekozZa <523H0114@student.tdtu.edu.vn>
2025-03-26 09:57:31 +0700

    complete main page

--- index.js
+++ index.js
@@ -12,26 +12,10 @@ app.get('/', (req, res) => {
     res.render('index.ejs')
 })
 
-app.get('/ingredients', (req, res) => {
-    res.render('./partials/ingredients.ejs')
+app.get('/searcher', (req, res) => {
+    res.render('./partials/searcher.ejs', {apiKey: process.env.API_KEY})
 })
 
-app.post('/meals', (req, res) => {
-    axios.get('https://api.spoonacular.com/recipes/findByIngredients', {
-        params: {
-            apiKey: process.env.API_KEY,
-            ingredients: req.body.ingredients,
-            number: 5
-        }
-    })
-    .then((response) => {
-        res.render('./partials/meals.ejs', {meals: response.data})
-    })
-    .catch((error) => {
-        console.log(error)
-    })
-    
-})
 
 app.listen(PORT, () => {
     console.log('http://localhost:3000/')
