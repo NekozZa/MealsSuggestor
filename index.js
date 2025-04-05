7d9f297e60852bae6fb6a1254d94fda9a4bb2bd9
Rookie <anhtuannguyenhuynh2005@gmail.com>
2025-04-05 20:06:19 +0700

    Login page

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import fs from 'fs'
 
 const app = express()
 const PORT = 3000
@@ -31,6 +32,99 @@ app.get('/profile', (req, res) => {
     res.render('profile.ejs')
 })
 
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
+})
+
+app.post('/login',(req, res) =>{
+    const user = req.body['username']
+    const passwordInput = req.body['password']
+    // console.log(req.body)
+    let data = {}
+    try{
+        const fileData = fs.readFileSync('accounts.json', 'utf8')
+        data = JSON.parse(fileData)
+    }catch(error){
+        console.log("File is not exits")
+    }
+    
+    if(!data.some(account => account.username === user)){
+        // console.log("Your username is not exits")
+        return res.json({errorMess: true});
+    }
+    const account = data.find(account => account.username === user);
+    
+    if(account.password !== passwordInput){
+        // console.log("Your password is wrong")
+        return res.json({errorMess: true});
+    }
+    if(account.role === "admin"){
+        return res.json({isAdmin: true});
+    }
+    const randomChar = account.apiKey
+    res.json({apiKey: randomChar})
+})
+
+app.post('/register', (req, res) => {
+    const user = req.body['username']
+    const email = req.body['email']
+    const passwordInput = req.body['password']
+    const roleSelected = req.body['role']
+
+    console.log(req.body)
+
+    let randomChar = randomString(10)
+    let data = []
+    try{
+        const fileData = fs.readFileSync('accounts.json', 'utf8')
+        data = JSON.parse(fileData)
+    }catch(error){
+        console.log("File is not exits")
+    }
+
+    if(user == null || email == null || passwordInput == null){
+        return res.json({errorMess: true});
+    }
+
+    if(data.some(account => account.username === user)){
+        // console.log("Your username is exits")
+        return res.json({errorMess: true});
+    }
+
+    if(passwordInput.length < 8){
+        return res.json({errorMess: true});
+    }
+    let id = data.length + 1
+
+    while(true){
+        if(data.find(account => account.id == id)){
+            id = id + 1
+        }else{
+            break
+        }
+    }
+    
+    if(roleSelected === "nutritionist"){
+        const interviewDate = req.body['interviewDate']
+        data.push({id:id, username:user, password: passwordInput, email: email, role: "normalcustomer", interviewDate: interviewDate}) 
+    }else{
+        data.push({id:id, username:user, password: passwordInput, email: email, role: roleSelected})
+    }
+    fs.writeFileSync('accounts.json', JSON.stringify(data, null, 2), 'utf8')
+    console.log("Successfull!")
+    // res.render('index.ejs', {apiKey: randomChar})
+    res.json({apiKey: randomChar})
+})
+
 app.listen(PORT, () => {
     console.log('http://localhost:3000/')
 })
+
+function randomString(length){
+    const char = 'abcdefghijklmnopqrstuvwxyz0123456789'
+    let res = ''
+    for(let i = 0; i < length; i++){
+        res += char.charAt(Math.floor(Math.random() * char.length))
+    }
+    return res
+}
